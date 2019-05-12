0cd0e3dd416d8da60e9ac2bd0126e4068f5e83ea
koiic <ibraismai1990@gmail>
2019-05-12 05:00:46 +0100

    add start script

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -23,10 +23,13 @@ module.exports = {
     },
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    username: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT = 3306,
+    define: {
+      timestamps: false
+    }
   }
 };
